9901059a3fddcb6a465dba23a3ce1957bf47a938
winson121 <winsonwijaya45@gmail.com>
2019-12-09 01:05:32 +0800

    complete the main functionality (delete, read, write, insert, print, quit) of the text editor

--- List/TextEditor.py
+++ List/TextEditor.py
@@ -3,47 +3,67 @@ from readFileToList import readFile
 
 class TextEditor(object):
     def __init__(self):
-        self._list = None
-    
-    def _listToString(self, list):
+        self._list = ArrayList()
+        self._list.append("")
+
+    def _listToString(self, strList):
         string = ""
-        for item in list:
-            string += item
+        for i in range(len(strList)):
+            string += strList[i]
         return string
 
-    def insert(self, index, str):
-        string = self._listToString(list):
-        self._list.insert(index, string)
+    def insert(self, lineIndex, strList):
+        lineIndex = int(lineIndex)
+        string = self._listToString(strList)
+        self._list.insert(lineIndex, string)
     
     def read(self, file):
         self._list = readFile(file)
+        if len(self._list) == 0:
+            self._list.append("")
     
-    def _print_list(self, num1=None, num2=None):
+    def _validPrintLine(self, index):
+        if not self._list._validIndex(index):
+            raise IndexError("Index is out of range")
+        line = index
+        if line < 0:
+            line = len(self._list) + line
+        return line
+
+    def print(self, num1=False, num2=False):
         if num1 and num2:
+            num1, num2 = int(num1), int(num2)
+            validNum1 = self._validPrintLine(num1)
+            validNum2 = self._validPrintLine(num2)
+            if not(validNum1 < validNum2):
+                raise IndexError("Index is out of range")
             for i in range(num1, num2+1):
                 print(self._list[i])
         elif num1:
+            num1 = int(num1)
             print(self._list[num1])
         elif not num1:
-            self._list.print()
+            print(self._list)
     
-    def write(self, file):
+    def write(self, filename):
         file = open(filename, "w+")
-        file.write("\n".join(self._list()))
+        file.write("\n".join(self._list))
         file.close()
     
-    def delete(self, line=None):
+    def delete(self, line):
+        if len(self._list) == 1:
+            raise IndexError("Can't delete anymore")
         if line:
-            self._list.remove(line)
+            self._list.delete(int(line))
         else:
-            self._list = ""
+            self.__init__()
     
     def search(self, word):
         lowCase = word.lower()
         lowCaseTxt = self._lowerCase()
         occurences = ArrayList()
         for i in range(len(lowCaseTxt)):
-            if word in lowCaseTxt[i]:
+            if lowCase in lowCaseTxt[i]:
                 occurences.append(i)
         return occurences
             
@@ -54,8 +74,35 @@ class TextEditor(object):
         
         return lower
     
-    def command(self, inputCommand):
+    def quit(self, arg):
+        exit()
+
+    def commandSelector(self, command, args):
+        commandDict = {
+            "insert": self.insert,
+            "read"  : self.read,
+            "write" : self.write,
+            "print" : self.print,
+            "delete": self.delete,
+            "search": self.search,            
+            "quit"  : self.quit
+        }
+        return commandDict[command](*args)
+
+    def command2(self, inputCommand):
         command = inputCommand.split()
-        if command:
-            if command[0] == "insert":
-                
+        args = [None] if command[-1] == command[0] else command[1:]
+        self.commandSelector(command[0], args)
+    
+    def main(self):
+        while True:
+            print(">>",end="")
+            option = input()
+            try:
+                self.command2(option)
+            except Exception:
+                print("?")
+
+if __name__ == "__main__":
+    editor = TextEditor()
+    editor.main()
